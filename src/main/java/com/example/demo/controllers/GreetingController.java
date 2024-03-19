1ecaa6f59129e111e33835777e715fc43eed19a8
Yarik Ozerskiy <ozerskiy.yaroslav@gmail.com>
2024-03-19 16:51:43 +0400

    add @ConfigurationProperties для использования данных из конфигурационного файла в коде

---
+++ src/main/java/com/example/demo/controllers/GreetingController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controllers;
+
+
+import com.example.demo.configuration.Greeting;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+
+    private final Greeting greeting;
+
+    public GreetingController(Greeting greeting) {
+        this.greeting = greeting;
+    }
+
+    @GetMapping
+    String getGreeting(){
+        return greeting.getName();
+    }
+
+    @GetMapping("/coffee")
+    String getNameAndCoffee(){
+        return greeting.getCoffee();
+    }
+
+}
